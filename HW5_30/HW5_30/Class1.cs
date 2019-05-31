e0845ca6b76f9945d7a058bed660d0fc6dbf4476
ALopez90 <50379823+ALopez90@users.noreply.github.com>
2019-05-31 12:31:09 -0500

    Add files via upload

---
+++ HW5_30/HW5_30/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW5_30
+{
+    public class Fruit
+    {
+        public string _name { set; get; }
+        public string _taste { set; get; }
+
+        public Fruit(string name, string taste)
+        {
+            _name = name;
+            _taste = taste;
+
+            Console.WriteLine("The fruit has finished growing! It's a {0}! These typically have a {1} taste.", name, taste);
+        }
+
+        public void Bunch(string type)
+        {
+            Console.WriteLine("This fruit is considered a {0}.", type);
+        }
+    }
+
+    internal class Berry : Fruit
+    {
+        public int _amount { set; get; }
+        public string _health { set; get; }
+
+        public Berry(string name, string taste, int amount) : base(name, taste)
+        {
+            _name = name;
+            _taste = taste;
+            _amount = amount;
+            _health = "Fruits are needed in one's diet to the many benefits; for example, it helsp by providing needed nutrients, and improving metabolism!";
+        }
+
+        public void eatFruit(int howmany)
+        {
+            Console.WriteLine("You decided to eat {0} {1}s", howmany, _name);
+        }
+    }
+}
---
+++ HW5_30/HW5_30/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW5_30
+{
+    /***Tonight's homework is more practice with classes and inheritance.
+            1. Create a new Console App project ***
+            2. Create a new class file by adding it to your project name (it's the same folder where Program.cs is in) ***
+            3. Your new class file will house your parent class and any subclasses that you create ***
+            4. Create a class called Fruit ***
+            5. Give the class two properties and one method ***
+            6. Then create a new class and name it any fruit you want ***
+            7. Make sure this class inherits from the Fruit class ***
+            8. Give this new class a new property (this will work with the other properties from the Fruit class) ***
+            9. For this new class create one method ***
+            10. After you instantiate the subclass (your new class, not the Fruit class), call one of the methods that you created in the Fruit class, then call the method you created in your subclass***/
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Berry fruit1 = new Berry("Banana", "sweet", 4);
+            fruit1.Bunch("berry");
+            fruit1.eatFruit(35);
+        }
+    }
+}
